e36b1390ebc15b41d389ddd8243c77220a7b13eb
madanlal mohanrao nikalje <madannikalje44@gmail.com>
2021-04-06 22:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask,render_template,request,url_for
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 import pickle
 import numpy as np
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/iris'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/iris'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-class t1(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    sepal_lenght = db.Column(db.Integer,nullable=False)
-    sepal_width = db.Column(db.Integer,nullable=False)
-    petal_lenght = db.Column(db.Integer,nullable=False)
-    petal_width = db.Column(db.Integer,nullable=False)
-    pred = db.Column(db.String(50),nullable=False)
+# class t1(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     sepal_lenght = db.Column(db.Integer,nullable=False)
+#     sepal_width = db.Column(db.Integer,nullable=False)
+#     petal_lenght = db.Column(db.Integer,nullable=False)
+#     petal_width = db.Column(db.Integer,nullable=False)
+#     pred = db.Column(db.String(50),nullable=False)
 
 f1 = open('enc','rb')
 f2 = open('sclr','rb')
@@ -36,9 +36,9 @@ def predict():
     scled = sclr.transform(arr)
     pred = clf.predict(scled)
 
-    entry = t1(sepal_lenght=vals[0],sepal_width=vals[1], petal_lenght=vals[2],petal_width=vals[3], pred=enc.inverse_transform(pred)[0])
-    db.session.add(entry)
-    db.session.commit()
+#     entry = t1(sepal_lenght=vals[0],sepal_width=vals[1], petal_lenght=vals[2],petal_width=vals[3], pred=enc.inverse_transform(pred)[0])
+#     db.session.add(entry)
+#     db.session.commit()
 
     return render_template('index.html',preds=enc.inverse_transform(pred)[0].upper(),vals=vals,scled=scled,pred=pred)
 
